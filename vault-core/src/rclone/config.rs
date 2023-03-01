c258dc30af516938ea5467daaa69893f9a9f2a07
Luka Zakrajšek <luka@bancek.net>
2023-03-01 17:07:08 +0100

    Normalize rclone config remote path

--- vault-core/src/rclone/config.rs
+++ vault-core/src/rclone/config.rs
@@ -4,11 +4,11 @@ use ini::Ini;
 use slug::slugify;
 use thiserror::Error;
 
-use crate::rclone::obscure::obscure;
+use crate::{rclone::obscure::obscure, utils::path_utils::normalize_path};
 
 use super::obscure::reveal;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Config {
     pub name: Option<String>,
     pub path: String,
@@ -52,6 +52,8 @@ pub fn parse_config(config_str: &str) -> Result<Config, ParseConfigError> {
         .split(":")
         .nth(1)
         .ok_or_else(|| ParseConfigError(String::from("remote missing path")))?;
+    let path =
+        normalize_path(path).map_err(|_| ParseConfigError(String::from("invalid remote path")))?;
     let obscured_password = props
         .get("password")
         .ok_or_else(|| ParseConfigError(String::from("missing password property")))?;
@@ -97,4 +99,148 @@ pub fn generate_config(config: &Config) -> String {
     i.write_to(&mut out).unwrap();
 
     String::from_utf8(out).unwrap()
+}
+
+#[cfg(test)]
+pub mod tests {
+    use regex::Regex;
+
+    use super::{generate_config, parse_config, Config};
+
+    #[test]
+    fn test_parse_config() {
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:/Vault\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\npassword2=8lJriKxI5ersT8JUSYG6jm2H-1q2HiiM\n")
+                .unwrap(),
+            Config {
+                name: Some(String::from("vault-name")),
+                path: String::from("/Vault"),
+                password: String::from("testpassword"),
+                salt: Some(String::from("testsalt")),
+            }
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:/Vault\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\n")
+                .unwrap(),
+            Config {
+                name: Some(String::from("vault-name")),
+                path: String::from("/Vault"),
+                password: String::from("testpassword"),
+                salt: None,
+            }
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:Vault\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\n")
+                .unwrap(),
+            Config {
+                name: Some(String::from("vault-name")),
+                path: String::from("/Vault"),
+                password: String::from("testpassword"),
+                salt: None,
+            }
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:/\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\n")
+                .unwrap(),
+            Config {
+                name: Some(String::from("vault-name")),
+                path: String::from("/"),
+                password: String::from("testpassword"),
+                salt: None,
+            }
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\n")
+                .unwrap(),
+            Config {
+                name: Some(String::from("vault-name")),
+                path: String::from("/"),
+                password: String::from("testpassword"),
+                salt: None,
+            }
+        );
+
+        assert_eq!(
+            parse_config("[vault-1]\ntype=crypt\nremote=koofr:/Vault\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\n\n[vault-2]\ntype=crypt\nremote=koofr:/Vault\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\n")
+                .unwrap_err()
+                .to_string(),
+            "parse config failed: expected config to have exactly 1 crypt section"
+        );
+
+        assert_eq!(
+            parse_config("").unwrap_err().to_string(),
+            "parse config failed: expected config to have exactly 1 crypt section"
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\npassword2=8lJriKxI5ersT8JUSYG6jm2H-1q2HiiM\n")
+                .unwrap_err()
+                .to_string(),
+            "parse config failed: missing remote property"
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:..\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\npassword2=8lJriKxI5ersT8JUSYG6jm2H-1q2HiiM\n")
+                .unwrap_err()
+                .to_string(),
+            "parse config failed: invalid remote path"
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:/Vault\n")
+                .unwrap_err()
+                .to_string(),
+            "parse config failed: missing password property"
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:/Vault\npassword=testpassword\n")
+                .unwrap_err()
+                .to_string(),
+            "parse config failed: failed to reveal password: obscure error: input too short when revealing password - is it obscured?"
+        );
+
+        assert_eq!(
+            parse_config("[vault-name]\ntype=crypt\nremote=koofr:/Vault\npassword=YMRulMcUAOo9raAGnYdie57EWnDFi_N283rEVw\npassword2=testsalt\n")
+                .unwrap_err()
+                .to_string(),
+            "parse config failed: failed to reveal password2: obscure error: input too short when revealing password - is it obscured?"
+        );
+    }
+
+    #[test]
+    fn test_generate_config() {
+        let config = generate_config(&Config {
+            name: Some(String::from("Vault name")),
+            path: String::from("/Vault"),
+            password: String::from("testpassword"),
+            salt: Some(String::from("testsalt")),
+        });
+        let expected =
+            "^\\[vault-name\\]\ntype=crypt\nremote=koofr:/Vault\npassword=.*\npassword2=.*\n$";
+        assert!(Regex::new(expected).unwrap().is_match(&config));
+
+        let config = generate_config(&Config {
+            name: None,
+            path: String::from("/Vault"),
+            password: String::from("testpassword"),
+            salt: Some(String::from("testsalt")),
+        });
+        let expected =
+            "^\\[vault\\]\ntype=crypt\nremote=koofr:/Vault\npassword=.*\npassword2=.*\n$";
+        assert!(Regex::new(expected).unwrap().is_match(&config));
+
+        let config = generate_config(&Config {
+            name: Some(String::from("Vault name")),
+            path: String::from("/Vault"),
+            password: String::from("testpassword"),
+            salt: None,
+        });
+        let expected = "^\\[vault-name\\]\ntype=crypt\nremote=koofr:/Vault\npassword=.*\n$";
+        assert!(Regex::new(expected).unwrap().is_match(&config));
+    }
 }
