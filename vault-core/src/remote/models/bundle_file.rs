a8e10538a8bc27fdc2bd7937709b18bd35ddff55
Luka Zakrajšek <luka@bancek.net>
2023-03-10 18:13:22 +0100

    Sort bundle file fields

--- vault-core/src/remote/models/bundle_file.rs
+++ vault-core/src/remote/models/bundle_file.rs
@@ -1,15 +1,17 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
 pub struct BundleFile {
+    pub name: String,
+    #[serde(rename = "type")]
+    pub typ: String,
+    pub modified: i64,
+    pub size: i64,
     #[serde(rename = "contentType")]
     pub content_type: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub hash: Option<String>,
-    pub modified: i64,
-    pub name: String,
-    pub size: i64,
-    pub tags: ::std::collections::HashMap<String, Vec<String>>,
-    #[serde(rename = "type")]
-    pub typ: String,
+    pub tags: HashMap<String, Vec<String>>,
 }
