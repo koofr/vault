d1270d8161ee086eded91ae14e8bc8e97dadbe06
Luka Zakrajšek <luka@bancek.net>
2023-04-14 17:14:57 +0200

    Add NavbarClose component

---
+++ vault-web/src/components/navbar/NavbarClose.tsx
@@ -0,0 +1,19 @@
+import { css } from '@emotion/css';
+import { memo } from 'react';
+import { Link, To } from 'react-router-dom';
+
+import { ReactComponent as NavbarCloseIcon } from '../../assets/images/navbar-close.svg';
+
+export const NavbarClose = memo<{ to: To }>(({ to }) => {
+  return (
+    <Link
+      to={to}
+      className={css`
+        display: flex;
+        align-items: center;
+      `}
+    >
+      <NavbarCloseIcon />
+    </Link>
+  );
+});
