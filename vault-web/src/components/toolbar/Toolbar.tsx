ab916a5afad3f8ab79e257d77d27d46887ddb63c
Luka Zakrajšek <luka@bancek.net>
2023-05-04 15:23:23 +0200

    Make ToolbarItem polymorphic

--- vault-web/src/components/toolbar/Toolbar.tsx
+++ vault-web/src/components/toolbar/Toolbar.tsx
@@ -1,7 +1,7 @@
 import { css, cx } from '@emotion/css';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
-import Button, { ButtonProps } from '@restart/ui/Button';
+import Button from '@restart/ui/Button';
 import { memo, ReactNode } from 'react';
 
 import { buttonReset } from '../../styles/mixins/buttons';
@@ -12,92 +12,110 @@ export const Toolbar = styled.nav`
   margin: 0 -12px 0 auto;
 `;
 
-export type ToolbarItemProps = {
+interface ToolbarItemBaseProps<T extends React.ElementType> {
+  as?: T;
   icon?: ReactNode;
   iconHover?: ReactNode;
   textClassName?: string;
-} & ButtonProps;
+}
 
-export const ToolbarItem = memo<ToolbarItemProps>(
-  ({ icon, iconHover, children, className, textClassName, ...props }) => {
-    const theme = useTheme();
+export type ToolbarItemProps<T extends React.ElementType = typeof Button> =
+  ToolbarItemBaseProps<T> &
+    Omit<React.ComponentPropsWithoutRef<T>, keyof ToolbarItemBaseProps<T>>;
 
-    return (
-      <Button
-        className={cx(
-          css`
-            ${buttonReset}
-            margin: 0 4px 0 12px;
-            padding: 0 8px 0 0;
-            text-align: left;
-          `,
-          className
-        )}
-        {...props}
+export function ToolbarItem<T extends React.ElementType = typeof Button>({
+  as,
+  icon,
+  iconHover,
+  className,
+  textClassName,
+  children,
+  ...props
+}: ToolbarItemProps<T>) {
+  const RootComponent = as || Button;
+  const theme = useTheme();
+
+  return (
+    <RootComponent
+      className={cx(
+        css`
+          ${buttonReset}
+          margin: 0 4px 0 12px;
+          padding: 0 8px 0 0;
+          text-align: left;
+
+          &[disabled] {
+            cursor: not-allowed;
+            pointer-events: none;
+            opacity: 0.7;
+          }
+        `,
+        className
+      )}
+      {...props}
+    >
+      <div
+        className={css`
+          display: flex;
+          align-items: center;
+        `}
       >
-        <div
-          className={css`
-            display: flex;
-            align-items: center;
-          `}
-        >
-          {icon !== undefined ? (
-            <div
-              className={cx(
-                css`
-                  display: flex;
-                  justify-content: center;
-                  align-items: center;
-                  width: 32px;
-                  height: 32px;
-                `,
-                iconHover !== undefined &&
-                  css`
-                    *:hover > * > & {
-                      display: none;
-                    }
-                  `
-              )}
-            >
-              {icon}
-            </div>
-          ) : null}
-          {iconHover !== undefined ? (
-            <div
-              className={css`
-                display: none;
+        {icon !== undefined ? (
+          <div
+            className={cx(
+              css`
+                display: flex;
                 justify-content: center;
                 align-items: center;
                 width: 32px;
                 height: 32px;
-
-                *:hover > * > & {
-                  display: flex;
-                }
-              `}
-            >
-              {iconHover}
-            </div>
-          ) : null}
-          <div
-            className={cx(
-              css`
-                font-size: 13px;
-                font-weight: 600;
-                color: ${theme.colors.text};
               `,
-              textClassName
+              iconHover !== undefined &&
+                css`
+                  *:hover > * > & {
+                    display: none;
+                  }
+                `
             )}
           >
-            {children}
+            {icon}
           </div>
-        </div>
-      </Button>
-    );
-  }
-);
+        ) : null}
+        {iconHover !== undefined ? (
+          <div
+            className={css`
+              display: none;
+              justify-content: center;
+              align-items: center;
+              width: 32px;
+              height: 32px;
 
-export const ToolbarCancelItem = memo<Omit<ToolbarItemProps, 'children'>>(
+              *:hover > * > & {
+                display: flex;
+              }
+            `}
+          >
+            {iconHover}
+          </div>
+        ) : null}
+        <div
+          className={cx(
+            css`
+              font-size: 13px;
+              font-weight: 600;
+              color: ${theme.colors.text};
+            `,
+            textClassName
+          )}
+        >
+          {children}
+        </div>
+      </div>
+    </RootComponent>
+  );
+}
+
+export const ToolbarCancelItem = memo<React.ComponentProps<typeof ToolbarItem>>(
   ({ textClassName, ...props }) => {
     const theme = useTheme();
 
