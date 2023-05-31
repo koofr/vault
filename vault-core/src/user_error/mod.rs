6908db05861de9077a7b9b7b7f63a3436774a904
Luka Zakrajšek <luka@bancek.net>
2023-05-31 12:42:33 +0200

    Add StringUserError

--- vault-core/src/user_error/mod.rs
+++ vault-core/src/user_error/mod.rs
@@ -3,3 +3,11 @@ pub use user_error_derive::UserError;
 pub trait UserError {
     fn user_error(&self) -> String;
 }
+
+pub struct StringUserError(pub String);
+
+impl UserError for StringUserError {
+    fn user_error(&self) -> String {
+        self.0.clone()
+    }
+}
