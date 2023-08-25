15e7a9f8429a6bd25b74aa92618f2c0ad6134f06
Luka Zakrajšek <luka@bancek.net>
2023-08-25 12:28:47 +0200

    Add delayed and abort response body to interceptor

--- vault-fake-remote/src/fake_remote/interceptor.rs
+++ vault-fake-remote/src/fake_remote/interceptor.rs
@@ -1,16 +1,45 @@
+use std::time::Duration;
+
 use axum::{
     extract::State,
     http::{request, Request},
     middleware::Next,
     response::Response,
 };
+use futures::future::BoxFuture;
+use vault_core::utils::{abort_http_body::AbortHttpBody, delayed_http_body::DelayedHttpBody};
 
 use super::app_state::AppState;
 
 pub enum InterceptorResult {
     Ignore,
     Transform(Box<dyn FnOnce(Response) -> Response + Send + Sync + 'static>),
+    AsyncTransform(
+        Box<dyn FnOnce(Response) -> BoxFuture<'static, Response> + Send + Sync + 'static>,
+    ),
     Response(Response),
+    AsyncResponse(BoxFuture<'static, Response>),
+}
+
+impl InterceptorResult {
+    pub fn delayed_response_body(duration: Duration) -> Self {
+        Self::Transform(Box::new(move |response| {
+            let (parts, body) = response.into_parts();
+            let body = DelayedHttpBody::unsync_box_body(body, tokio::time::sleep(duration));
+            Response::from_parts(parts, body)
+        }))
+    }
+
+    pub fn delayed_abort_response_body(duration: Duration) -> Self {
+        Self::Transform(Box::new(move |response| {
+            let (parts, _) = response.into_parts();
+            let body = AbortHttpBody::new(|| {
+                axum::Error::new(std::io::Error::from(std::io::ErrorKind::BrokenPipe))
+            });
+            let body = DelayedHttpBody::unsync_box_body(body, tokio::time::sleep(duration));
+            Response::from_parts(parts, body)
+        }))
+    }
 }
 
 pub type Interceptor = Box<dyn Fn(&request::Parts) -> InterceptorResult + Send + Sync + 'static>;
@@ -31,7 +60,11 @@ pub async fn interceptor_middleware<B>(
             match res {
                 InterceptorResult::Ignore => next.run(request).await,
                 InterceptorResult::Transform(transform) => transform(next.run(request).await),
+                InterceptorResult::AsyncTransform(transform) => {
+                    transform(next.run(request).await).await
+                }
                 InterceptorResult::Response(response) => response,
+                InterceptorResult::AsyncResponse(response) => response.await,
             }
         }
         None => next.run(request).await,
