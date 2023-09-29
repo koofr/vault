d86adc5b5c56ea3a11d45d81b2725946435992b9
Luka Zakrajšek <luka@bancek.net>
2023-09-29 10:48:27 +0200

    Add log to core tests

--- vault-core-tests/src/helpers/mod.rs
+++ vault-core-tests/src/helpers/mod.rs
@@ -11,17 +11,30 @@ use crate::fixtures::{
     vault_fixture::VaultFixture,
 };
 
+pub fn with_base(f: impl FnOnce()) {
+    let mut env_logger_builder = env_logger::Builder::from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
+    env_logger_builder.filter_module("vault_fake_remote", log::LevelFilter::Warn);
+    env_logger_builder.filter_module("vault_core", log::LevelFilter::Info);
+    let _ = env_logger_builder.try_init();
+
+    f()
+}
+
 pub fn with_tokio_runtime(
     f: impl FnOnce(Arc<tokio::runtime::Runtime>) -> BoxFuture<'static, ()> + Send + Sync + 'static,
 ) {
-    let tokio_runtime = Arc::new(tokio::runtime::Runtime::new().unwrap());
+    with_base(|| {
+        let tokio_runtime = Arc::new(tokio::runtime::Runtime::new().unwrap());
 
-    tokio_runtime.clone().block_on(
-        async move {
-            f(tokio_runtime).await;
-        }
-        .boxed(),
-    );
+        tokio_runtime.clone().block_on(
+            async move {
+                f(tokio_runtime).await;
+            }
+            .boxed(),
+        );
+    });
 }
 
 pub fn with_fake_remote(
