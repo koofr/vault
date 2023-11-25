dfa0dc4a24c539b9b266cbdb2dffa6d2ab49e1ba
Luka Zakrajšek <luka@bancek.net>
2023-11-25 09:21:34 +0100

    Add sync encrypt reader

--- vault-core/src/cipher/encrypt_reader.rs
+++ vault-core/src/cipher/encrypt_reader.rs
@@ -1,5 +1,10 @@
 use core::mem;
-use std::{cmp, io::Result, pin::Pin, sync::Arc};
+use std::{
+    cmp,
+    io::{Read, Result},
+    pin::Pin,
+    sync::Arc,
+};
 
 use futures::{
     ready,
@@ -35,6 +40,106 @@ pub enum EncryptReaderState {
         buffer: Vec<u8>,
         pos: usize,
     },
+}
+
+pub struct SyncEncryptReader<R> {
+    inner: R,
+    state: EncryptReaderState,
+    data_cipher: Arc<XSalsa20Poly1305>,
+}
+
+impl<R> SyncEncryptReader<R> {
+    pub fn new(inner: R, data_cipher: Arc<XSalsa20Poly1305>, nonce: Nonce) -> Self {
+        Self {
+            inner,
+            state: EncryptReaderState::WritingMagic { nonce, pos: 0 },
+            data_cipher,
+        }
+    }
+}
+
+impl<R: Read> Read for SyncEncryptReader<R> {
+    fn read(self: &mut Self, buf: &mut [u8]) -> Result<usize> {
+        loop {
+            match &mut self.state {
+                EncryptReaderState::WritingMagic { nonce, pos } => {
+                    let n = cmp::min(buf.len(), FILE_MAGIC_SIZE - *pos);
+
+                    buf[..n].copy_from_slice(&FILE_MAGIC[*pos..*pos + n]);
+
+                    *pos += n;
+
+                    if *pos == FILE_MAGIC_SIZE {
+                        self.state = EncryptReaderState::WritingNonce {
+                            nonce: mem::take(nonce),
+                            pos: 0,
+                        };
+                    }
+
+                    return Ok(n);
+                }
+                EncryptReaderState::WritingNonce { nonce, pos } => {
+                    let n = cmp::min(buf.len(), FILE_NONCE_SIZE - *pos);
+
+                    buf[..n].copy_from_slice(&nonce.as_slice()[*pos..*pos + n]);
+
+                    *pos += n;
+
+                    if *pos == FILE_NONCE_SIZE {
+                        self.state = EncryptReaderState::ReadingPlaintext {
+                            nonce: mem::take(nonce),
+                            buffer: vec![0; BLOCK_DATA_SIZE],
+                            pos: 0,
+                        };
+                    }
+
+                    return Ok(n);
+                }
+                EncryptReaderState::ReadingPlaintext { nonce, buffer, pos } => {
+                    let n = self.inner.read(&mut buffer[*pos..])?;
+
+                    if n == 0 && *pos == 0 {
+                        return Ok(0);
+                    }
+
+                    *pos += n;
+
+                    if n == 0 || *pos == BLOCK_DATA_SIZE {
+                        let encrypted =
+                            match encrypt_block(&self.data_cipher, &nonce, &buffer[..*pos]) {
+                                Ok(encrypted) => encrypted,
+                                Err(e) => return Err(e.into()),
+                            };
+
+                        nonce.increment();
+
+                        self.state = EncryptReaderState::WritingCiphertext {
+                            nonce: mem::take(nonce),
+                            buffer: encrypted,
+                            pos: 0,
+                        };
+                    }
+                }
+                EncryptReaderState::WritingCiphertext { nonce, buffer, pos } => {
+                    let n = cmp::min(buf.len(), buffer.len() - *pos);
+
+                    buf[..n].copy_from_slice(&buffer[*pos..*pos + n]);
+
+                    *pos += n;
+
+                    if *pos == buffer.len() {
+                        self.state = EncryptReaderState::ReadingPlaintext {
+                            nonce: mem::take(nonce),
+                            buffer: vec![0; BLOCK_DATA_SIZE],
+                            pos: 0,
+                        };
+                    }
+
+                    return Ok(n);
+                }
+            }
+        }
+    }
 }
 
 pin_project! {
@@ -150,12 +255,13 @@ impl<R: AsyncRead> AsyncRead for EncryptReader<R> {
 mod tests {
     use std::{io::Result, sync::Arc, task::Poll};
 
-    use futures::{channel::mpsc, stream::TryStreamExt, AsyncRead};
+    use futures::{stream::TryStreamExt, AsyncRead};
     use xsalsa20poly1305::XSalsa20Poly1305;
 
     use crate::cipher::{
         constants::FILE_MAGIC,
         data_cipher::{decrypt_block, get_data_cipher},
+        encrypt_reader::SyncEncryptReader,
         nonce::Nonce,
         test_helpers::{assert_reader_pending, assert_reader_ready},
     };
@@ -186,11 +292,58 @@ mod tests {
     }
 
     #[test]
+    fn test_sync_encrypt_reader() {
+        let data_cipher = get_dummy_data_cipher();
+        let nonce = get_dummy_nonce();
+
+        let reader = std::io::Cursor::new(b"test".to_vec());
+
+        let mut r = SyncEncryptReader::new(reader, data_cipher.clone(), nonce.clone());
+
+        fn read(r: &mut impl std::io::Read, n: usize) -> Result<Vec<u8>> {
+            let mut buf = vec![0; n];
+            r.read(&mut buf).map(|n| {
+                buf.truncate(n);
+                buf
+            })
+        }
+
+        let res1 = read(&mut r, 7).unwrap();
+        assert_eq!(res1.len(), 7);
+        let res2 = read(&mut r, 7).unwrap();
+        assert_eq!(res2.len(), 1);
+        let res = concat_vecs(&mut [res1, res2]);
+
+        assert_eq!(res, FILE_MAGIC);
+
+        let res1 = read(&mut r, 23).unwrap();
+        assert_eq!(res1.len(), 23);
+        let res2 = read(&mut r, 23).unwrap();
+        assert_eq!(res2.len(), 1);
+        let res = concat_vecs(&mut [res1, res2]);
+
+        assert_eq!(res, nonce.as_slice().to_vec());
+
+        let res1 = read(&mut r, 18).unwrap();
+        assert_eq!(res1.len(), 18);
+        let res2 = read(&mut r, 18).unwrap();
+        assert_eq!(res2.len(), 2);
+        let res = concat_vecs(&mut [res1, res2]);
+
+        let plaintext = decrypt_block(&data_cipher, &nonce, &res).unwrap();
+
+        assert_eq!(plaintext, b"test");
+
+        let res = read(&mut r, 10000);
+        assert_eq!(res.unwrap().len(), 0);
+    }
+
+    #[test]
     fn test_encrypt_reader() {
         let data_cipher = get_dummy_data_cipher();
         let nonce = get_dummy_nonce();
 
-        let (tx, rx) = mpsc::unbounded::<Result<Vec<u8>>>();
+        let (tx, rx) = futures::channel::mpsc::unbounded::<Result<Vec<u8>>>();
         let reader = rx.into_async_read();
 
         let mut r = EncryptReader::new(reader, data_cipher.clone(), nonce.clone());
