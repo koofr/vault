d6a2339000fa2bd369ccde90d49ccf426b0bf986
Luka Zakrajšek <luka@bancek.net>
2025-12-28 11:14:17 +0100

    Upgrade directories and dirs-sys
    Move back to upstream lib

--- vault-desktop-server/src/data_path.rs
+++ vault-desktop-server/src/data_path.rs
@@ -9,14 +9,14 @@ pub struct GetDataPathError(String);
 pub fn get_data_path(app_id: &str) -> Result<PathBuf, GetDataPathError> {
     #[cfg(not(windows))]
     let data_dir = {
-        let home_dir = dirs_sys_next::home_dir()
+        let home_dir = dirs_sys::home_dir()
             .ok_or_else(|| GetDataPathError(format!("failed to get home dir")))?;
 
         home_dir.join(format!(".{}", app_id))
     };
     #[cfg(windows)]
     let data_dir = {
-        let project_dirs = directories_next::ProjectDirs::from("", "", app_id)
+        let project_dirs = directories::ProjectDirs::from("", "", app_id)
             .ok_or_else(|| GetDataPathError(format!("failed to find project dirs for app")))?;
 
         project_dirs.data_dir().to_path_buf()
