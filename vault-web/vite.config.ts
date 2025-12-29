b3ee0748704338dfd6be24c15415933be3dc190a
Luka Zakrajšek <luka@bancek.net>
2025-12-29 18:01:16 +0100

    Add explicit input to vite config
    This was added to prevent dependency scanning in public/ folder which includes pdf.js html files and causes false positives

--- vault-web/vite.config.ts
+++ vault-web/vite.config.ts
@@ -5,4 +5,9 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      input: ['index.html'],
+    },
+  },
 });
